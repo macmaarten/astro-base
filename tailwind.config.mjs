825a0a18b1ba610e37b4cf109c73022a9d5b7c40
Maarten <github@xpa.nl>
2026-01-31 17:04:34 +0100

    Further commoit for dark mode
    Finetuned dark mode plus switcher, still not perfect, but before I go fiddling with other thins I'll commit this. Yay!

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,15 @@ import typography from "@tailwindcss/typography";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        dark: {
+          base: "rgb(14, 26, 53)",    // main page background
+          surface: "rgb(23, 38, 73)", // header / footer / cards
+        },
+      },
+    },
+  },
   plugins: [typography],
 };
